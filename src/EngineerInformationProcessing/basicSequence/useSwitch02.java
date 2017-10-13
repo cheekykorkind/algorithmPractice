f00ece6ed5e18bc8a599985ad8c61419ddc0c292
cheekyorkind <gkbn7y7h>
2017-10-13 15:04:18 +0900

    switch02

---
+++ src/EngineerInformationProcessing/basicSequence/useSwitch02.java
@@ -0,0 +1,23 @@
+package EngineerInformationProcessing.basicSequence;
+/*
+ * 문제 : -(1/2)+(2/3)-(4/5)....-(99/100)까지의 합계를 구하라
+ * 1. 홀수번째는 - 한다. 짝수번째는 + 한다.
+ * 2. 100까지 반복한다.
+ * 3. 결과값을 쌓아야 한다.
+ */
+public class useSwitch02 {
+	public useSwitch02(){
+		float i = 0, result = 0;
+		while(i < 100){
+			if((int)(i/2) == i/2){	//	짝수번째
+				result += i/(i+1);
+				i++;
+			}else{	//	홀수번째
+				result -= i/(i+1);
+				i++;
+			}			
+		}
+		System.out.printf("%f", result);
+	}
+	
+}
--- src/testingPlace/Main.java
+++ src/testingPlace/Main.java
@@ -7,13 +7,12 @@ import sort.BinarySearch;
 import sort.SelectionSort;
 import sort.InsertionSort;
 import EngineerInformationProcessing.basicSequence.useSwitch01;
+import EngineerInformationProcessing.basicSequence.useSwitch02;
 import FactoryMethodPattern.CoffeeFactory;
 import FactoryMethodPattern.FactoryMethodMain;
 
 public class Main {
 	public static void main(String[] args) {
-		new useSwitch01();
+		new useSwitch02();
 	}	//	End of main method
 }	//	End of Main class
-
-//new DijkstraAlgorithm();
